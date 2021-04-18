0f472fb7dc1b2b6bc3d8b48de20799e079178080
陈嘉璇 <44282360+chenjianxua@users.noreply.github.com>
2021-04-18 09:41:40 +0800

    Update menu-data.js

--- notepad/js/menu-data.js
+++ notepad/js/menu-data.js
@@ -2,34 +2,34 @@
 np.menuData = [{
   title: '文件(F)',
   menuItems: [
-    {title: '新建(N)',shortcut: 'Ctrl+N'}, 
-    {title: '打开(O)...',shortcut: 'Ctrl+O',},
-    {title: '保存(S)',shortcut: 'Ctrl+S',enabled: true}, 
-    {title: '另存为(A)...',shortcut: '',enabled: true}, 
-    {title: 'hr',shortcut: '',enabled: true}, 
-    {title: '页面设置(U)...',shortcut: '',enabled: true}, 
-    {title: '打印(P)...',shortcut: 'Ctrl+P',enabled: true},
+    {title: '新建(N)',shortcut: 'Ctrl+N', handler: () => {console.log('1')}}, 
+    {title: '打开(O)...',shortcut: 'Ctrl+O',handler: () => {console.log('1')}},
+    {title: '保存(S)',shortcut: 'Ctrl+S',enabled: true,handler: () => {console.log('1')}}, 
+    {title: '另存为(A)...',shortcut: '',enabled: true,handler: () => {console.log('1')}}, 
+    {title: 'hr',shortcut: '',enabled: true,handler: () => {console.log('1')}}, 
+    {title: '页面设置(U)...',shortcut: '',enabled: true,handler: () => {console.log('1')}}, 
+    {title: '打印(P)...',shortcut: 'Ctrl+P',enabled: true,handler: () => {console.log('1')}},
     {title: 'hr',shortcut: '',enabled: true,handler: null}, 
-    {title: '退出(X)',shortcut: '',enabled: true}
+    {title: '退出(X)',shortcut: '',enabled: true,handler: () => {console.log('1')}}
   ],
   width: '202px',
   left: '0px'
 }, {
   title: '编辑(E)',
   menuItems: [
-    {title: '撤销(U)', shortcut: 'Ctrl+Z',enabled: false}, 
-    {title: 'hr',shortcut: '',enabled: true}, 
-    {title: '剪切(T)',shortcut: 'Ctrl+X',enabled: true},
-    {title: '复制(C)',shortcut: 'Ctrl+C',enabled: false}, 
-    {title: '粘贴(P)',shortcut: 'Ctrl+V', enabled: false},
-    {title: '删除(L)',shortcut: 'Del',enabled: false}, 
+    {title: '撤销(U)', shortcut: 'Ctrl+Z',enabled: false,handler: null}, 
+    {title: 'hr',shortcut: '',enabled: true,handler: null}, 
+    {title: '剪切(T)',shortcut: 'Ctrl+X',enabled: true,handler: null},
+    {title: '复制(C)',shortcut: 'Ctrl+C',enabled: false,handler: null}, 
+    {title: '粘贴(P)',shortcut: 'Ctrl+V', enabled: false,handler: null},
+    {title: '删除(L)',shortcut: 'Del',enabled: false,handler: null}, 
 
     ],
   width: '218px',
   left: '52px'
 }, {
   title: '格式(O)',
-  menuItems: [{title: '自动换行(W)', shortcut: '',enabled: true}, 
+  menuItems: [{title: '自动换行(W)', shortcut: '',enabled: true,handler: null}, 
     {title: '字体(F)...',shortcut: '',enabled: true,
     handler: () => $dlgFont.show({
       family: np.fontFamily,
@@ -61,9 +61,10 @@ np.menuData = [{
 }, {
   title: '帮助(H)',
   menuItems: [
-    {title: '查看帮助(H)',shortcut: '',enabled: true}, 
-    {title: '关于记事本(A)',shortcut: '',enabled: true}
+    {title: '查看帮助(H)',shortcut: '',enabled: true,handler: null}, 
+    {title: '关于记事本(A)',shortcut: '',enabled: true,handler: null}
   ],
   width: '166px',
   left: '216px'
 }];
+
